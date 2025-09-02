1fe64504cb0cae30f2c9bb9bf9aba317cb7fb948
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-09-02 15:07:50 +0600

    date function

---
+++ date.js
@@ -0,0 +1,10 @@
+let date = new Date();
+console.log(date.toString());
+console.log(date.toTimeString());
+console.log(date.toLocaleDateString());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.getSeconds());
--- typeConversion.js
+++ typeConversion.js
@@ -1,14 +1,19 @@
 // string to number and number to string
-// also convert in boolean to number and vice versaa
+// also convert in boolean to number and vice versa
+
 let str = "123";
 let num = 123;
 
 console.log(str + 1); // "1231"
 console.log(num + 1); // 124
 
-console.log(str + num); // "123123"
+console.log(str + num); // "123123" + operator has a dual purpose: 
+                        // addition for numbers and concatenation for strings.
+
 console.log(str - num); // 0
-console.log(str * num); // 15129
+
+console.log(str * num); // 15129 * is a purely mathematical operation
+                        // JavaScript tries to make sense of the operation in a mathematical context.
 
 console.log(str / num); // 1
 console.log(str * 2); // 246
