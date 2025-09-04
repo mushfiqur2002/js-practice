d2b7f83cb0632b61fd84d7af766cd37ea6999c36
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-09-04 15:01:23 +0600

    string

---
+++ string.js
@@ -0,0 +1,9 @@
+// string constructor and string methods
+
+let str1 = new String(123); // not recommended
+let str2 = String(true);
+let str3 = String(null);
+let str4 = String(undefined);
+
+console.log(str1, str2, str3, str4);
+console.log(typeof str1, typeof str2, typeof str3, typeof str4);
