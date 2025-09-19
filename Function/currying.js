9414b8fa9465ba6193f287caf528fec64ddea862
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-09-19 19:40:38 +0600

    currying

---
+++ Function/currying.js
@@ -0,0 +1,14 @@
+function add(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        }
+    }
+}
+
+function addHudai(a, b, c) {
+    return a + b + c;
+}
+let result = add(5)(6)()
+console.log(result);
+console.log(addHudai(5, 6 ));
